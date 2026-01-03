659c947adf1172cc4f0bb36a3e9cf5aaa09f3b4a
yeonjinJoo <jooyj0529@naver.com>
2026-01-04 03:17:39 +0900

    refactor: Router에서 진행하던 handler 선정을 HandlerMapping 클래스로 분리

--- src/main/java/webserver/Router.java
+++ src/main/java/webserver/Router.java
@@ -1,30 +1,15 @@
 package webserver;
 
+import application.handler.Handler;
 import webserver.http.HTTPRequest;
 import webserver.http.HTTPResponse;
 
 public class Router {
 
-    public static HTTPResponse handle(HTTPRequest request) {
-        return switch (request.getMethod()){
-            case "GET" -> handleGetRequest(request);
-//            case "POST" -> handlePostRequest(request);
-            default -> null;
-        };
-    }
-
-    private static HTTPResponse handleGetRequest(HTTPRequest request){
+    public static HTTPResponse route(HTTPRequest request) {
         String path = request.getPath();
-        String resourcePath = path;
+        Handler handler = HandlerMapping.getProperHandler(path);
 
-        if(path.equals("/")){
-            return StaticFileServer.serve("/index.html");
-        }
-
-        if(path.equals("/registration")){
-            resourcePath += "/index.html";
-        }
-
-        return StaticFileServer.serve(resourcePath);
+        return handler.handle(request);
     }
 }
