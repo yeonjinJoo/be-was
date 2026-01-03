cf4871cc8f9f503221aa6dabf82dce4183772418
yeonjinJoo <jooyj0529@naver.com>
2026-01-04 03:05:01 +0900

    refactor: request status와 message 선언을 HTTPStatus enum에서 하도록 변경

---
+++ src/main/java/webserver/http/HTTPStatus.java
@@ -0,0 +1,19 @@
+package webserver.http;
+
+public enum HTTPStatus {
+    OK(200, "OK"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed");
+
+    private final int code;
+    private final String message;
+
+    HTTPStatus(int code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public int code(){return code;}
+    public String meesage(){return message;}
+}
