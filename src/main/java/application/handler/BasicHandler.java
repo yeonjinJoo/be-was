92327d2f6f5d38131bbeb225dc28068955bf164e
yeonjinJoo <jooyj0529@naver.com>
2026-01-04 04:38:33 +0900

    feat: RegistrationHandler, BasicHandler handle 함수 구현 완료

--- src/main/java/application/handler/BasicHandler.java
+++ src/main/java/application/handler/BasicHandler.java
@@ -1,4 +1,15 @@
 package application.handler;
 
+import webserver.StaticFileServer;
+import webserver.http.HTTPRequest;
+import webserver.http.HTTPResponse;
+
 public class BasicHandler implements Handler{
+    @Override
+    public HTTPResponse handle(HTTPRequest request) {
+        String path = request.getPath();
+        if(path.equals("/")){ path += "/index.html"; }
+
+        return StaticFileServer.serve(path);
+    }
 }
--- src/main/java/application/handler/RegistrationHandler.java
+++ src/main/java/application/handler/RegistrationHandler.java
@@ -1,4 +1,20 @@
 package application.handler;
 
+import webserver.StaticFileServer;
+import webserver.http.HTTPRequest;
+import webserver.http.HTTPResponse;
+import webserver.http.HTTPStatus;
+
 public class RegistrationHandler implements Handler{
+    @Override
+    public HTTPResponse handle(HTTPRequest request) {
+        String path = request.getPath();
+
+        if(path.equals("/registration")){
+            return StaticFileServer.serve(path + "/index.html");
+        }
+
+        HTTPStatus httpStatus = HTTPStatus.METHOD_NOT_ALLOWED;
+        return new HTTPResponse(httpStatus.code(), httpStatus.meesage(), httpStatus.byteMessage());
+    }
 }
