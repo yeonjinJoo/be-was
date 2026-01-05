c8e95225a02fc113ae21eac0ec37367c14b0d90c
yeonjinJoo <jooyj0529@naver.com>
2026-01-05 16:41:13 +0900

    feat: UserService에 user create 기능 추가

---
+++ src/main/java/application/service/UserService.java
@@ -0,0 +1,22 @@
+package application.service;
+
+import application.db.UserDatabase;
+import application.model.User;
+
+public class UserService {
+    private final UserDatabase userDatabase = new UserDatabase();
+
+    public void create(String userId, String password, String name, String email) {
+        validDuplicateUser(userId);
+
+        User user = new User(userId, password, name, email);
+        userDatabase.addUser(user);
+    }
+
+    private void validDuplicateUser(String userId) {
+        userDatabase.findUserById(userId)
+                .ifPresent(message -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
