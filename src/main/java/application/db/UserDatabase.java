9cc52b94c06e76f0ea555c5eb68d301a0e3c5704
yeonjinJoo <jooyj0529@naver.com>
2026-01-05 16:44:58 +0900

    fix: 동시성 이슈 방어를 위해 UserDatabase의 users를 HashMap -> ConcurrentHashMap으로 변경

--- src/main/java/application/db/UserDatabase.java
+++ src/main/java/application/db/UserDatabase.java
@@ -3,18 +3,18 @@ package application.db;
 import application.model.User;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class Database {
-    private static Map<String, User> users = new HashMap<>();
+public class UserDatabase {
+    private static ConcurrentHashMap<String, User> users = new ConcurrentHashMap<String, User>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
     }
 
-    public static User findUserById(String userId) {
-        return users.get(userId);
+    public static Optional<User> findUserById(String userId) {
+        return Optional.ofNullable(users.get(userId));
     }
 
     public static Collection<User> findAll() {
