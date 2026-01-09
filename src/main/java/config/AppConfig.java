281a0012a988b42173c2403cb2cd34c40fc857f4
yeonjinJoo <jooyj0529@naver.com>
2026-01-09 19:05:14 +0900

    feat: 추가된 Handler와 Interceptor 초기화 코드 추가

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -2,15 +2,18 @@ package config;
 
 import application.db.UserDatabase;
 import application.handler.UserCreateHandler;
-import application.handler.UserLoginController;
+import application.handler.UserLoginHandler;
+import application.handler.UserLogoutHandler;
 import application.service.UserService;
 import webserver.HandlerMapping;
 import webserver.RouteKey;
-import webserver.Router;
+import webserver.Dispatcher;
 import webserver.handler.Handler;
 import webserver.handler.StaticFileHandler;
 import webserver.http.HTTPMethod;
-import webserver.session.SessionManager;
+import session.SessionManager;
+import webserver.interceptor.InterceptorRegistry;
+import webserver.interceptor.LoginCheckInterceptor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,23 +27,39 @@ public class AppConfig {
 
     private final StaticFileHandler staticFileHandler = new StaticFileHandler();
     private final UserCreateHandler userCreateHandler = new UserCreateHandler(userService);
-    private final UserLoginController userLoginController = new UserLoginController(userService, sessionManager);
+    private final UserLoginHandler userLoginHandler = new UserLoginHandler(userService, sessionManager);
+    private final UserLogoutHandler userLogoutHandler = new UserLogoutHandler(sessionManager);
+
+    private final LoginCheckInterceptor loginCheckInterceptor = new LoginCheckInterceptor(sessionManager);
 
     private final HandlerMapping handlerMapping = new HandlerMapping(handleMap(), staticFileHandler);
 
-    public AppConfig(){
+    private final InterceptorRegistry interceptorRegistry = configureInterceptors();
+
+    public AppConfig() {
         router();
     }
 
-    public Router router(){
-        return new Router(handlerMapping, sessionManager);
+    public Dispatcher router() {
+        return new Dispatcher(handlerMapping, interceptorRegistry);
+    }
+
+    private InterceptorRegistry configureInterceptors() {
+        InterceptorRegistry registry = new InterceptorRegistry();
+
+        // 로그인 체크 인터셉터 등록 및 경로 설정
+        registry.addInterceptor(loginCheckInterceptor)
+                .addPathPatterns("/mypage/**", "/user/logout"); // 로그인이 필요한 페이지들
+
+        return registry;
     }
 
     // Handlers
-    private Map<RouteKey, Handler> handleMap(){
+    private Map<RouteKey, Handler> handleMap() {
         Map<RouteKey, Handler> map = new HashMap<>();
         map.put(new RouteKey(HTTPMethod.POST, "/user/create"), userCreateHandler);
-        map.put(new RouteKey(HTTPMethod.POST, "/user/login"), userLoginController);
+        map.put(new RouteKey(HTTPMethod.POST, "/user/login"), userLoginHandler);
+        map.put(new RouteKey(HTTPMethod.POST, "/user/logout"), userLogoutHandler);
         return map;
     }
 }
