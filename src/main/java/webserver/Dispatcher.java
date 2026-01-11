c6d5281a7ce17d3709121b1aef3f0e0b99571bbe
yeonjinJoo <jooyj0529@naver.com>
2026-01-11 18:38:02 +0900

    feat: 로그인 실패 시 알림 기능 추가

--- src/main/java/webserver/Dispatcher.java
+++ src/main/java/webserver/Dispatcher.java
@@ -3,6 +3,7 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.exception.BadRequestException;
+import webserver.exception.ConflictException;
 import webserver.exception.UnauthorizedException;
 import webserver.exception.WebException;
 import webserver.http.HTTPMethod;
@@ -12,6 +13,8 @@ import webserver.http.HTTPResponse;
 import webserver.interceptor.Interceptor;
 import webserver.interceptor.InterceptorRegistry;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class Dispatcher {
@@ -50,8 +53,10 @@ public class Dispatcher {
 
     public HTTPResponse handleWebException(WebException e, HTTPRequest request){
         if(e instanceof UnauthorizedException ||
-                e instanceof BadRequestException){
-            return HTTPResponse.redirect("/login");
+                e instanceof BadRequestException ||
+                e instanceof ConflictException){
+            String encodedMessage = URLEncoder.encode(e.getMessage(), StandardCharsets.UTF_8);
+            return HTTPResponse.redirect("/login/index.html?error=true&message=" + encodedMessage);
         }
 
         return HTTPResponse.error(e.getStatus(), e.getMessage());
