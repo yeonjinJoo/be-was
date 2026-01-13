dab78f8c1c4af67e694fe241692d0bda82aeff6e
yeonjinJoo <jooyj0529@naver.com>
2026-01-13 11:00:55 +0900

    feat: Repository를 interface로 분리

--- src/main/java/application/repository/PostRepository.java
+++ src/main/java/application/repository/PostRepository.java
@@ -1,28 +1,7 @@
 package application.repository;
 
 import application.model.Post;
-import config.DBConfig;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public class PostRepository {
-    public void addPost(Post post){
-        String sql = "INSERT INTO post(content, userId) VALUES (?, ?)";
-
-        try(Connection conn = DBConfig.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)){
-            pstmt.setString(1, post.getContent());
-            pstmt.setString(2, post.getUserId());
-
-            int affected = pstmt.executeUpdate();
-            if (affected != 1) {
-                throw new RuntimeException("게시글 저장 실패: affectedRows=" + affected);
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
-            throw new RuntimeException("데이터베이스 접근 에러 발생");
-        }
-    }
+public interface PostRepository {
+    public void addPost(Post post);
 }
